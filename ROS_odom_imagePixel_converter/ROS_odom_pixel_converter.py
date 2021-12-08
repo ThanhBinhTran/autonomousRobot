a104cbeec3d692efaa6a0475c2c99f3d4c2f9b01
ThanhBinhTran <thanhbinh.hcmut@gmail.com>
2021-12-08 22:49:46 +0700

    update ReadME

--- ROS_odom_imagePixel_converter/ROS_odom_pixel_converter.py
+++ ROS_odom_imagePixel_converter/ROS_odom_pixel_converter.py
@@ -4,12 +4,17 @@ import matplotlib.pyplot as plt
 from ROS_read_yaml import read_yaml
 from ROS_read_pgm import read_pgm
 
+''' user input '''
+import argparse
+
+
+
 # declare filenames
 #map_name = "turtlebot3_house"
 #map_name = "turtlebot3_world"
-map_name = 'turtlebot3_world_full'
-yaml_file = map_name + ".yaml"
-map_file = map_name + ".pgm"
+#map_name = 'turtlebot3_world_full'
+#yaml_file = map_name + ".yaml"
+#map_file = map_name + ".pgm"
 
 # using term coordinate or point (pt) to denote location in gazebo world
 # using pixel (short px) to denote location in map image
@@ -31,7 +36,25 @@ def cal_origin_coordinate_pixel(img_central_pixel, origin):
     # convert ogirin coordinate of xyz ,where is at middle of gazebo world (0,0,0), to image pixel
     return [img_central_pixel[0] - origin[0], img_central_pixel[1] + origin[1]]
 
+''' get user input '''
+def menu():
+    parser = argparse.ArgumentParser(description='odometry pose and image pixel converter.')
+    parser.add_argument('-sm', help='slam map', default='turtlebot3_world_full')
+    parser.add_argument('-ox', type=float, help='(odometry) pose: x',default=-2.0)
+    parser.add_argument('-oy', type=float, help='(odometry) pose: y',default=-0.5)
+    parser.add_argument('-px', type=float, help='(next point) pixel: x',default=200.0)
+    parser.add_argument('-py', type=float, help='(next point) pixel: y',default=150.0)
+    args = parser.parse_args()
+    print ("___________________")
+    print (args)
+
+    return args
 if __name__ == "__main__":
+    ''' get user input'''
+    userin = menu()
+    slam_map = userin.sm
+    yaml_file = slam_map + ".yaml"
+    map_file = slam_map + ".pgm"
 
     ''' read map image '''
     image = read_pgm(map_file, byteorder='<')
@@ -53,8 +76,7 @@ if __name__ == "__main__":
     origin_coordinate_px = cal_origin_coordinate_pixel(imgc_px, origin)
     
     # get odometry pose info (x = -2, y = - 0.5, z = 0)  then convert to pixel
-    odom_pose = [-2, -0.5, 0]   # get from odom topic (replace this info by /odom topic)
-    #odom_pose = [-3, 1, 0]      # get from odom topic (replace this info by /odom topic house)
+    odom_pose = [userin.ox, userin.oy]   # get from odom topic (replace this info by /odom topic)
     print ("odom_pose ", odom_pose)
 
     # calculate robot position in pixel
@@ -62,17 +84,19 @@ if __name__ == "__main__":
     print ("robot_coordinate", robot_px)
 
     ''' move to next point in image pixel '''
-    nxtpt_px = [190, 135]
+    next_pt_px = [userin.px, userin.py]
     # calculate next odom pose in gazebo world
-    nxt_odom_pose = pixel_to_odom(origin_coordinate_px, resolution, nxtpt_px)
+    nxt_odom_pose = pixel_to_odom(origin_coordinate_px, resolution, next_pt_px)
 
 
     # show map and robot position
+    fig = plt.figure(figsize=(5, 5))
+    fig.canvas.set_window_title('Odometry information and image pixel converter')
     plt.imshow(image, plt.cm.gray)
     points = np.array([ imgc_px,                 # middle pixel in image
                         origin_coordinate_px,    # origin cooridate (0,0,0) in image
                         robot_px,                # robot in image
-                        nxtpt_px,                 # move to next point in image  
+                        next_pt_px,              # move to next point in image  
                         [0,0],
                         [w,0],
                         [0,h],
