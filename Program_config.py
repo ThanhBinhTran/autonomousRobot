f072b1b43fed2bc3e64234b59bb0cf8e74dd8812
ThanhBinhTran <thanhbinh.hcmut@gmail.com>
2022-03-16 08:08:27 +0700

    refactoring parameter

--- Program_config.py
+++ Program_config.py
@@ -1,6 +1,3 @@
-import math
-from enum import Enum
-
 '''
 Parameters to show/hide element(s) of animation 
 '''
@@ -12,16 +9,14 @@ show_traversalSights = True
 
 show_openSight = True
 show_closedSight = True
-show_circleRange = False
-
-show_refSight = False
+show_circleRange = True
 
 show_sketelonPath = True
 show_approximately_shortest_path = True
-show_critical_line_segments = False
+show_critical_line_segments = True
 show_cls_orderednumber = True  # show ordered number of critical line segments
 
-show_visitedPath = False
+show_visitedPath = True
 show_visibilityGraph = True
 
 show_local_openpt = True
@@ -32,10 +27,12 @@ show_robot = True
 show_goal = True
 show_start = True
 show_text_goal = True
+
 '''
     transparent 
 '''
-transparent_alpha = 0.1
+transparent = 0.1
+
 '''
 Assumption of Hoai An Theory.
 '''
@@ -45,16 +42,16 @@ THEORY_AR_ALGORITHMS = True
 show_active_openpt_HA_Assumption = True         # active open points (HA Assumption)
 show_activeArc_HA_Assumption = True             # arc points (HA Assumption)
 show_parentArc_HA_Assumption = True             # parent arc points (HA Assumption)
-show_boundaryPts_HA_Assumption = False          # boundary points intersection between circle and obstacles
+show_boundaryPts_HA_Assumption = True           # boundary points intersection between circle and obstacles
 show_circleGoal_HA_Assumption = True            # big circle (at goal within radius = dist(goal, robot center))
-show_activeArcPts_order_HA_Assumption = False   # show order of arc points according to its angles
+show_activeArcPts_order_HA_Assumption = True    # show order of arc points according to its angles
 # line styles
 ls_bp_HA = "k."     # boundary points (HA Assumption)
 ls_aap_HA = "c."    # active arc points (HA Assumption)
 ls_pap_HA = "go"    # parent arc points (HA Assumption)
 
 '''
-PRINT OUT FOR DEBUG
+parementers to hide/show information
 '''
 print_boundary_line_segments = False
 print_closed_sights = False
@@ -89,73 +86,3 @@ ls_goingp = "-1g"  # going path
 ls_cls = '--g'  # critical line segment
 ls_sp = "-r"  # shortest skeleton path
 ls_asp = "-m"  # approximately shortest path
-
-
-# Robots configuration
-class RobotType(Enum):
-    circle = 0
-    rectangle = 1
-
-class Config:
-    '''
-    simulation parameter class
-    '''
-
-    def __init__(self):
-        # robot parameter
-        self.max_speed = 100.0  # [m/s]
-        self.min_speed = -100.0  # [m/s]
-        self.max_yaw_rate = 40.0 * math.pi / 180.0  # [rad/s]
-        self.max_accel = 0.2  # [m/ss]
-        self.max_delta_yaw_rate = 40.0 * math.pi / 180.0  # [rad/ss]
-        self.v_resolution = 0.01  # [m/s]
-        self.yaw_rate_resolution = 0.1 * math.pi / 180.0  # [rad/s]
-        self.dt = 0.1  # [s] Time tick for motion prediction
-        self.predict_time = 0.1  # [s]
-        self.to_goal_cost_gain = 0.15
-        self.speed_cost_gain = 1.0
-        self.obstacle_cost_gain = 1.0
-        self.robot_stuck_flag_cons = 0.001  # constant to prevent robot stucked
-        self.robot_type = RobotType.circle
-        self.robot_vision = 20  # the range of input vision
-        
-        # if robot_type == RobotType.circle
-        # Also used to check if goal is reached in both types
-        self.robot_radius = 1.0  # [m] for collision check
-
-        # if robot_type == RobotType.rectangle
-        self.robot_width = 0.5  # [m] for collision check
-        self.robot_length = 1.2  # [m] for collision check
-
-        self.max_speed = 1.0  # [m/s]
-        self.min_speed = -0.5  # [m/s]
-        self.max_yaw_rate = 40.0 * math.pi / 180.0  # [rad/s]
-        self.max_accel = 0.2  # [m/ss]
-        self.max_delta_yaw_rate = 40.0 * math.pi / 180.0  # [rad/ss]
-        self.v_resolution = 0.01  # [m/s]
-        self.yaw_rate_resolution = 0.1 * math.pi / 180.0  # [rad/s]
-        self.dt = 0.1  # [s] Time tick for motion prediction
-        self.predict_time = 3.0  # [s]
-        self.to_goal_cost_gain = 0.15
-        self.speed_cost_gain = 1.0
-        self.obstacle_cost_gain = 1.0
-        self.robot_stuck_flag_cons = 0.001  # constant to prevent robot stucked
-        self.robot_type = RobotType.circle
-
-        # if robot_type == RobotType.circle
-        # Also used to check if goal is reached in both types
-        self.robot_radius = 1.0  # [m] for collision check
-
-        # if robot_type == RobotType.rectangle
-        self.robot_width = 0.5  # [m] for collision check
-        self.robot_length = 1.2  # [m] for collision check
-
-    @property
-    def robot_type(self):
-        return self._robot_type
-
-    @robot_type.setter
-    def robot_type(self, value):
-        if not isinstance(value, RobotType):
-            raise TypeError("robot_type must be an instance of RobotType")
-        self._robot_type = value
